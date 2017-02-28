e75c6f03e1af691e0dd18f145fd041f573b767ca
Vittorio <vittorio.cadastro@gmail.com>
2017-02-28 15:02:10 -0300

    deployment

--- config/env/development.js
+++ config/env/development.js
@@ -3,7 +3,9 @@
  */
 module.exports = {
     sessionSecret: 'mSecret',
-    db: 'mongodb://localhost/treino_impeua'
+    db: 'mongodb://vmlb:Utld2287@ds145289.mlab.com:45289/impeua'
+
 };
+// db: 'mongodb://localhost/treino_impeua'
 // db: 'mongodb://vmlb:Utld2287@ds145289.mlab.com:45289/impeua'
 // db: 'mongodb://localhost/treino_impeua'
